41ca73ce2b129fd47e27cfbf7765229b91e6d86a
Joe Lau <mcfa77y@gmail.com>
2014-11-19 18:35:22 -0800

    add simple routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res) {
+//   res.render('index', { title: 'Express' });
+// });
+
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Joe Lau\'s App',
+                       age: 33});
+//   res.send({users: ['will', 'laura']});
 });
+router.get('/get', function(req, res) {
+  console.log(req.query);
+});
+
+router.get('/users/:id', function(req, res) {
+  console.log(req.params);
+  res.send(req.params.id, 200);
+});
+
 
 module.exports = router;
